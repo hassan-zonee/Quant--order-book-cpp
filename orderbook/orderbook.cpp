3a87af2ab62707c5c859b5b157c37ff5069dbcd2
Hassan <hassan.zoneee@gmail.com>
2025-11-10 10:47:02 +0000

    working on orderbook class

--- orderbook/orderbook.cpp
+++ orderbook/orderbook.cpp
@@ -3,6 +3,8 @@
 #include<vector>
 #include<string>
 #include<list>
+#include<map>
+#include<unordered_map>
 using namespace std;
 
 
@@ -61,6 +63,8 @@ public:
     Quantity GetRemainingQuantity() const { return remainingQuantity_; }
     Quantity GetFilledQuantity() const { return GetInitialQuantity() - GetRemainingQuantity(); }
     
+    bool IsFilled() const { return GetRemainingQuantity() == 0; }
+
     void Fill(Quantity quantity)
     {
         if (quantity > GetRemainingQuantity()) {
@@ -131,7 +135,82 @@ using Trades = vector<Trade>;
 class OrderBook {
 private:
 
+    struct OrderEntry {
+        OrderPointer order_{ nullptr };
+        OrderPointers::iterator location_;
+    };
 
+    map<Price, OrderPointers, greater<Price>> bids_;
+    map<Price, OrderPointers, less<Price>> asks_;
+    unordered_map<OrderId, OrderEntry> orders_;
+
+    bool CanMatch(Side side, Price price) const
+    {
+        if (side == Side::Buy)
+        {
+            if (asks_.empty()) return false;
+
+            const auto& [bestAsk, _] = *asks_.begin();
+            return price >= bestAsk;
+        }
+        else
+        {
+            if (bids_.empty()) return false;
+
+            const auto& [bestBid, _] = *bids_.begin();
+            return price <= bestBid; 
+        }
+    }
+
+    Trades MatchOrders()
+    {
+        Trades trades;
+        trades.reserve(orders_.size());
+
+        while (true)
+        {
+            if (bids_.empty() || asks_.empty())
+            {
+                break;
+            }
+
+            auto& [bidPrice, bids] = *bids_.begin();
+            auto& [askPrice, asks] = *asks_.begin();
+
+            if (bidPrice < askPrice) break;
+
+            while (bids.size() && asks.size()) 
+            {
+                auto& bid = bids.front();
+                auto& ask = asks.front();
+
+                Quantity quantity = min(bid->GetRemainingQuantity(), ask->GetRemainingQuantity());
+                bid->Fill(quantity);
+                ask->Fill(quantity);
+
+                if (bid->IsFilled())
+                {
+                    bids.pop_front();
+                    orders_.erase(bid->GetOrderId());
+                }
+                if (ask->IsFilled())
+                {
+                    asks.pop_front();
+                    orders_.erase(ask->GetOrderId());
+                }
+
+                if (bids.empty()) bids_.erase(bidPrice);
+                if (asks.empty()) asks_.erase(askPrice);
+
+                trades.push_back(
+                    Trade{
+                        TradeInfo{bid->GetOrderId(), bid->GetPrice(), quantity},
+                        TradeInfo{ask->GetOrderId(), ask->GetPrice(), quantity}
+                    }
+                );
+            }
+        }
+    }
 };
 
 int main()
